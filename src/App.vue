138cec7e19254177ace9ec85791f1bbab4c994dc
zhuzhenglin <1522499825@qq.com>
2022-01-09 16:47:05 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
 <style>
-@import "assets/css/base.css";
+  @import "assets/css/base.css";
 </style>
